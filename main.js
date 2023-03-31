2be2bb56eab8492bd10ecf97260be3abbf8a108f
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-03-31 14:29:34 +0900

    Implement #01 navbar

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('.navbar-purple-x').addEventListener('click',function(){
+    document.querySelector('.navbar-purple').element.classList.add('close')
+});
+
+document.querySelector('.cust-center-modal').addEventListener('mouseover',function(){
+    document.querySelector('.').display='block'
+});
