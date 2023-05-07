9f8155ea713c29f827e7f6a4c6219c346c5241b8
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-05-08 00:13:20 +0900

    Update #06
    justify-content: 
    center/flex-start/flex-end/space-between/space-evenly...
    align-items: 
    center/flex-start/flex-end

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ document.querySelector('#customer-center').addEventListener('mouseout',function(
         document.querySelector('#cust-center').style.display='none';
     }
 })
+
+document.querySelector('.navbar_close').addEventListener('onclick', function(){
+    document.querySelector('#navbar_wrap').display='none';
+});
+document.querySelector('#beautyCurly').addEventListener('mouseover',function(){
+    document.querySelector('#beautyCurly').style.color='rgb(95, 0, 128)';
+});
+document.querySelector('#beautyCurly').addEventListener('mouseout',function(){
+    document.querySelector('#beautyCurly').style.color='rgb(181, 181, 181)';
+});
+
+
+document.querySelectorAll('.categori_list').addEventListener('mouseover',function(){
+    document.querySelectorAll('.categori_list').style.color='rgb(95, 0, 128)';
+    document.querySelectorAll('.categori_list').style.textDecoration='underline';
+});
+document.querySelectorAll('.categori_list').addEventListener('mouseout',function(){
+    document.querySelectorAll('.categori_list').style.color='black';
+    document.querySelectorAll('.categori_list').style.textDecoration='none';
+});
